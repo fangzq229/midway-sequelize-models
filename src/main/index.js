05e27d5ca8f3acbdd1ab06f2c52f1f4edfb9ab97
fangzhenqi <m18811126599@163.com>
2020-08-26 10:49:37 +0800

    mac 快捷键解决

--- src/main/index.js
+++ src/main/index.js
@@ -17,21 +17,20 @@ const winURL = process.env.NODE_ENV === 'development'
 
 function createWindow() {
   if (process.platform === 'darwin') {
-    const template = [
-      {
-        label: 'Application',
-        submenu: [
-          { label: 'Quit', accelerator: 'Command+Q', click: function () { app.quit(); } }
-        ]
-      },
-      {
-        label: 'Edit',
-        submenu: [
-          { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-          { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' }
-        ]
-      }
-    ];
+    template.unshift({
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' }
+      ]
+    })
     Menu.setApplicationMenu(Menu.buildFromTemplate(template))
   } else {
     Menu.setApplicationMenu(null)
